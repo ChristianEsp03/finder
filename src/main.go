2cb9ad95cd7e9209cacc1f47b86360ad85416b2c
Christian <chris.ese2003@gmail.com>
2024-04-26 11:15:09 +0200

    minor improvement

--- src/main.go
+++ src/main.go
@@ -8,21 +8,15 @@ import (
 	"strings"
 )
 
-func funcErr(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-
 // get all drive in the pc
 func getdrives() (r []string) {
 	for _, drive := range "ABCDEFGHIJKLMNOPQRSTUVWXYZ" {
 		f, err := os.Open(string(drive) + ":\\")
-		
+
 		if err == nil {
-			r = append(r, string(drive) + ":\\")
+			r = append(r, string(drive)+":\\")
 		}
-		
+
 		defer f.Close()
 	}
 	return
@@ -30,7 +24,7 @@ func getdrives() (r []string) {
 
 func usage() {
 	fmt.Print(
-`usage: find [<name>] [<type>] [<optional>]
+		`usage: find [<name>] [<type>] [<optional>]
 
 in the 'type' field insert:
   -f	to find a file
@@ -44,19 +38,19 @@ in the 'optional' field you can put the name of single drive
 func findFile(elem string, items []fs.DirEntry, path string) {
 	for _, obj := range items {
 		fullpath := filepath.Join(path, obj.Name())
-		
+
 		if !obj.IsDir() {
 			if strings.Contains(obj.Name(), elem) {
 				fmt.Println(fullpath)
 			}
 		} else {
 			newItems, err := os.ReadDir(fullpath)
-			
+
 			if err != nil {
 				continue
 			}
-			
-			findFile(elem, newItems, fullpath)			
+
+			findFile(elem, newItems, fullpath)
 		}
 	}
 }
@@ -64,18 +58,18 @@ func findFile(elem string, items []fs.DirEntry, path string) {
 func findDir(elem string, items []fs.DirEntry, path string) {
 	for _, obj := range items {
 		fullpath := filepath.Join(path, obj.Name())
-		
+
 		if obj.IsDir() {
 			if strings.Contains(obj.Name(), elem) {
 				fmt.Println(fullpath)
 			}
-			
+
 			newItems, err := os.ReadDir(fullpath)
-			
+
 			if err != nil {
 				continue
 			}
-			
+
 			findDir(elem, newItems, fullpath)
 		}
 	}
@@ -84,55 +78,74 @@ func findDir(elem string, items []fs.DirEntry, path string) {
 func main() {
 	// start := time.Now()
 	drives := getdrives()
+	argLen := len(os.Args)
 
-	if len(os.Args) == 1 {
+	if argLen == 1 {
 		usage()
 		os.Exit(1)
-	}
+	} else if argLen == 2 {
+		usage()
+		os.Exit(1)
+	} else {
+		elem, elem_type := os.Args[1], os.Args[2]
+		fmt.Println("Searching...")
 
-	elem, elem_type := os.Args[1], os.Args[2]
-	fmt.Println("Ricerca...")
-
-	switch elem_type {
+		switch elem_type {
 		case "-f":
-			if len(os.Args) < 4 {
+			if argLen < 4 {
 				for _, drive := range drives {
 					items, err := os.ReadDir(drive)
-					funcErr(err)
+
+					if err != nil {
+						fmt.Println("Error searching file")
+					}
+
 					findFile(elem, items, drive)
 				}
 			} else {
 				for _, drive := range drives {
 					if strings.EqualFold(os.Args[3], drive) {
 						items, err := os.ReadDir(drive)
-						funcErr(err)
+
+						if err != nil {
+							fmt.Println("Error searching file")
+						}
+
 						findFile(elem, items, drive)
 					}
 				}
 			}
-			
+
 		case "-d":
-			if len(os.Args) < 4 {
+			if argLen < 4 {
 				for _, drive := range drives {
 					items, err := os.ReadDir(drive)
-					funcErr(err)
+
+					if err != nil {
+						fmt.Println("Error searching directory")
+					}
+
 					findDir(elem, items, drive)
 				}
 			} else {
 				for _, drive := range drives {
 					if strings.EqualFold(os.Args[3], drive) {
 						items, err := os.ReadDir(drive)
-						funcErr(err)
+
+						if err != nil {
+							fmt.Println("Error searching directory")
+						}
+
 						findDir(elem, items, drive)
 					}
 				}
 			}
-			
+
 		default:
 			usage()
+		}
 	}
-	
+
 	// finish := time.Since(start)
 	// fmt.Println(finish)
 }
-
